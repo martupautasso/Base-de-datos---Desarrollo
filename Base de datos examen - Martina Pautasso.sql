a164452ea1e246dbf54ef4bb34de45bea82bdba4
martupautasso <80895255+martupautasso@users.noreply.github.com>
2021-07-06 20:41:34 -0300

    Add files via upload

---
+++ Base de datos examen - Martina Pautasso.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE examen1;
+USE examen1;
+CREATE TABLE estudiantess (
+idest int  not null primary key,
+nombre varchar(12) not null,
+apellido varchar(12) not null,
+nota int not null,
+curso varchar(40) not null);
